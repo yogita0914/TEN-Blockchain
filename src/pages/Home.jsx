ecdbcd70469e08aa1fe58ff97164bc45cafeb1d9
Chiefofstaff03 <marketing@entrepreneurshipnetwork.net>
2025-01-30 11:26:43 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,8 @@ export function Home() {
   const featureCardRef = useRef(null);
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    // <div className="min-h-screen bg-gray-100">
+       <div className="min-h-screen bg-blue-100">
       <Hero featureCardRef={featureCardRef}/>
       <Hero2 />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
